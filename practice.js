dfa1ca9ac5dbfb30438cb4a67211c1d057a441fc
hochi0187 <58836648+hochi0187@users.noreply.github.com>
2020-01-14 12:06:51 +0900

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,166 @@
+
+
+//네비게이션 메뉴 애니메이션-
+
+var nav = $('nav');
+var line = $('<div />').addClass('line');
+
+line.appendTo(nav);
+
+var active = nav.find('.active');
+var pos = 0;
+var wid = 0;
+
+if(active.length) {
+  pos = active.position().left;
+  wid = active.width();
+  line.css({
+    left: pos,
+    width: wid
+  });
+}
+
+nav.find('ul li a').click(function(e) {
+  e.preventDefault();
+  if(!$(this).parent().hasClass('active') && !nav.hasClass('animate')) {
+    
+    nav.addClass('animate');
+
+    var _this = $(this);
+
+    nav.find('ul li').removeClass('active');
+
+    var position = _this.parent().position();
+    var width = _this.parent().width();
+
+    if(position.left >= pos) {
+      line.animate({
+        width: ((position.left - pos) + width)
+      }, 300, function() {
+        line.animate({
+          width: width,
+          left: position.left
+        }, 150, function() {
+          nav.removeClass('animate');
+        });
+        _this.parent().addClass('active');
+      });
+    } else {
+      line.animate({
+        left: position.left,
+        width: ((pos - position.left) + wid)
+      }, 300, function() {
+        line.animate({
+          width: width
+        }, 150, function() {
+          nav.removeClass('animate');
+        });
+        _this.parent().addClass('active');
+      });
+    }
+
+    pos = position.left;
+    wid = width;
+  }
+});
+
+
+function maxOrbit(x,y) {
+  var max = Math.max(x,y),
+      diameter = Math.round(Math.sqrt(max*max + max*max));
+  return diameter/2;
+}
+
+var Star = function() {
+
+  this.orbitRadius = random(maxOrbit(w,h));
+  this.radius = random(60, this.orbitRadius) / 12;
+  this.orbitX = w / 2;
+  this.orbitY = h / 2;
+  this.timePassed = random(0, maxStars);
+  this.speed = random(this.orbitRadius) / 50000;
+  this.alpha = random(2, 10) / 10;
+
+  count++;
+  stars[count] = this;
+}
+
+Star.prototype.draw = function() {
+  var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
+      y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
+      twinkle = random(10);
+
+  if (twinkle === 1 && this.alpha > 0) {
+    this.alpha -= 0.05;
+  } else if (twinkle === 2 && this.alpha < 1) {
+    this.alpha += 0.05;
+  }
+
+  ctx.globalAlpha = this.alpha;
+    ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
+  this.timePassed += this.speed;
+}
+
+for (var i = 0; i < maxStars; i++) {
+  new Star();
+}
+
+function animation() {
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.globalAlpha = 0.8;
+    ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 1)';
+    ctx.fillRect(0, 0, w, h)
+  
+  ctx.globalCompositeOperation = 'lighter';
+  for (var i = 1, l = stars.length; i < l; i++) {
+    stars[i].draw();
+  };  
+  
+  window.requestAnimationFrame(animation);
+}
+
+animation();
+
+
+(function () {
+  $('.skills-prog li').find('.skills-bar').each(function (i) {
+    $(this).find('.bar').delay(i * 150).animate({
+      width: $(this).parents().attr('data-percent') + '%' },
+    1000, 'linear', function () {
+      return $(this).css({
+        'transition-duration': '.5s' });
+
+    });
+  });
+
+  $('.skills-soft li').find('svg').each(function (i) {
+    var c, cbar, circle, percent, r;
+    circle = $(this).children('.cbar');
+    r = circle.attr('r');
+    c = Math.PI * (r * 2);
+    percent = $(this).parent().data('percent');
+    cbar = (100 - percent) / 100 * c;
+    circle.css({
+      'stroke-dashoffset': c,
+      'stroke-dasharray': c });
+
+    circle.delay(i * 150).animate({
+      strokeDashoffset: cbar },
+    1000, 'linear', function () {
+      return circle.css({
+        'transition-duration': '.3s' });
+
+    });
+    $(this).siblings('small').prop('Counter', 0).delay(i * 150).animate({
+      Counter: percent },
+    {
+      duration: 1000,
+      step: function (now) {
+        return $(this).text(Math.ceil(now) + '%');
+      } });
+
+  });
+
+}).call(this);
+
+
